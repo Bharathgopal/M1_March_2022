173939176b9fb0d871b8b44f28c0677e3268fd56
Shaikhhuda <101114060+Shaikhhuda@users.noreply.github.com>
2022-08-17 22:25:19 +0530

    Solution for activity-1

---
+++ mfilecreator.sh
@@ -0,0 +1,12 @@
+ #!\bin\sh
+# put current date as yyyy-mm-dd in $date
+#YYYY is year
+#MM is month
+#DD is date
+date=$ date "+%Y-%m-%d"
+#put current time as hh-mm in $date
+#HH is hour in 24 hour format
+#MM is Minutes
+date=$ date "+%H:%M"
+
+
